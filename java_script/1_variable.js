e5968142351bd4c317faafd425efb89085972703
HARE <136250756+HARE-07@users.noreply.github.com>
2025-05-31 07:45:00 +0000

    change done

---
+++ java_script/1_variable.js
@@ -0,0 +1,13 @@
+let accountId ="9abace"
+var accountpassword="hare@baby.com"
+const id = 3443
+// id = 444 // it can't change.
+let account_sate // it will be unidefined
+
+/* perefer not to use var due to prbl in block scope and function dcope*/
+
+account_city ="Delhi"
+ accountpassword = "dfff"
+ accountId = "1234"
+
+ console.table([accountpassword,accountId,id,account_city,account_sate])
